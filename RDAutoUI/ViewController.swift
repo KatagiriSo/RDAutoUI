da74d51446666e39766071f3b9b3cbf815d7df76
katagiri <so@katagiri>
2017-05-10 15:22:15 +0900

    Initial Commit

---
+++ RDAutoUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RDAutoUI
+//
+//  Created by 片桐奏羽 on 2017/05/10.
+//  Copyright © 2017年 rodhos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
